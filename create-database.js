e54d62af4bbe7acbc5e215850e3cf56454db6b62
cruz4 <cruz@raccoon.ag>
2020-08-06 08:27:47 -0300

    Criando tabelas para o banco de dados MySQL

--- create-database.js
+++ create-database.js
@@ -10,5 +10,78 @@ const connection = mysql.createConnection({
 connection.connect(function(err){
   if(err) return console.log(err);
   console.log('Conectou com sucesso');
+  createTableBrands(connection);
+  createTableModels(connection);
+  createTableVehicles(connection);
+  console.log("Tabelas criadas com sucesso")
 });
 
+function createTableBrands(conn){
+ 
+      const sql = "CREATE TABLE IF NOT EXISTS Brands (\n"+
+                  "ID int NOT NULL AUTO_INCREMENT,\n"+
+                  "name varchar(150) NOT NULL,\n"+
+                  "PRIMARY KEY (ID)\n"+
+                  ");";
+      
+      conn.query(sql, function (error, results, fields){
+          if(error) return console.log(error);
+          console.log('Tabela criada com sucesso!');
+      });
+}
+
+function createTableBrands(conn){
+ 
+    const sql = "CREATE TABLE IF NOT EXISTS Brands (\n"+
+                "ID int NOT NULL AUTO_INCREMENT,\n"+
+                "name varchar(150) NOT NULL,\n"+
+                "PRIMARY KEY (ID)\n"+
+                ");";
+    
+    conn.query(sql, function (error, results, fields){
+        if(error) return console.log(error);
+        console.log('Tabela criada com sucesso!');
+    });
+}
+
+function createTableBrands(conn){
+ 
+    const sql = "CREATE TABLE IF NOT EXISTS Brands (\n"+
+                "ID int NOT NULL AUTO_INCREMENT,\n"+
+                "name varchar(150) NOT NULL,\n"+
+                "PRIMARY KEY (ID)\n"+
+                ");";
+    
+    conn.query(sql, function (error, results, fields){
+        if(error) return console.log(error);
+        console.log('Tabela criada com sucesso!');
+    });
+}
+
+function createTableModels(conn){
+ 
+    const sql = "CREATE TABLE IF NOT EXISTS Models (\n"+
+                "ID int NOT NULL AUTO_INCREMENT,\n"+
+                "name varchar(150) NOT NULL,\n"+
+                "PRIMARY KEY (ID)\n"+
+                ");";
+    
+    conn.query(sql, function (error, results, fields){
+        if(error) return console.log(error);
+        console.log('Tabela criada com sucesso!');
+    });
+}
+
+function createTableVehicles(conn){
+ 
+    const sql = "CREATE TABLE IF NOT EXISTS Vehicles (\n"+
+                "ID int NOT NULL AUTO_INCREMENT,\n"+
+                "name varchar(150) NOT NULL,\n"+
+                "PRIMARY KEY (ID)\n"+
+                ");";
+    
+    conn.query(sql, function (error, results, fields){
+        if(error) return console.log(error);
+        console.log('Tabela criada com sucesso!');
+    });
+}
