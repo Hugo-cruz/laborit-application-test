14e8ff906d1d43bb280bec0fcf3c536a89263685
cruz4 <cruz@raccoon.ag>
2020-08-05 21:06:10 -0300

    Inserindo todos os gets do CRUD, ainda sem retornar os dados

--- index.js
+++ index.js
@@ -3,6 +3,35 @@ const server = express();
 
 server.listen(3000);
 
-server.get('/teste_inicial', () => {
-    console.log("Node inicializado com sucesso");
-})
+// ######## CRUD ###########
+//------ gets -----------------------
+//Brands
+server.get('/api/brands/:id', function(req,res) {
+    console.log("Gets para marcas");
+    console.log(req.params.id);
+});
+
+server.get('/api/brands', function(req,res) {
+    console.log("Get para todas as marcas");
+});
+
+//Models
+server.get('/api/models/:id', function(req,res) {
+    console.log("Gets para models");
+    console.log(req.params.id);
+});
+
+server.get('/api/models', function(req,res) {
+    console.log("Gets para todos os models");
+});
+
+//Vehicles
+server.get('/api/vehicles/:id', function(req,res) {
+    console.log("Gets para Vehicles");
+    console.log(req.params.id);
+});
+
+server.get('/api/vehicles', function(req,res) {
+    console.log("Gets para todos os Vehicles");
+    console.log(req.params.id);
+});
